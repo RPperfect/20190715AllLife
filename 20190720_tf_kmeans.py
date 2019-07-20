8e643124b3aeb7c9316b0b4eef93f8eab48eeb21
RPperfect <48400981+RPperfect@users.noreply.github.com>
2019-07-20 21:49:41 +0800

    Update 20190720_tf_kmeans.py

--- 20190720_tf_kmeans.py
+++ 20190720_tf_kmeans.py
@@ -43,3 +43,38 @@ sess.run(centroids)
 rep_centroids=tf.reshape(tf.tile(centroids,[N,1]),[N,K,2])
 rep_points=tf.reshape(tf.tile(points,[1,K]),[N,K,2])
 sum_squares=tf.reduce_sum(tf.square(rep_points-rep_centroids),reduction_indices=2)
+best_centroids=tf.argmin(sum_squares,1)
+did_assignments_change=tf.reduce_any(tf.not_equal(best_centroids,cluster_assignments))
+
+def bucket_mean(data,bucket_ids,num_buckets):
+    total=tf.unsorted_segment_sum(data,bucket_ids,num_buckets)
+    count=tf.unsorted_segment_sum(tf.ones_like(data),bucket_ids,num_buckets)
+    return total/count
+    
+means=bucket_mean(points,best_centroids,k)
+with tf.control_dependencies([did_assignments_change]):
+    do_updates=tf.group(centroids.assign(means),cluster_assignments.assign(best_centroids))
+
+changed=True
+iters=0
+fig,ax=plt.subplots()
+if DATA_TYPE=='blobs':
+    colourindexes=[2,1,4,3]
+else:
+    colourindexes=[2,1]
+while changed and iters<MAX_ITERS:
+    fig,ax=plt.subplots()
+    iters+=1
+    [changed,_]=sess.run([did_assignments_change,do_updates])
+    [centers,assignments]=sess.run([centroids,cluster_assignments])
+    ax.scatter(sess.run(points).transpose()[0],sess.run(points).transpose()[1],marker='o',s=200,c=assignments,cmap=plt.cm.coolwarm)
+    ax.scatter(centers[:,0],centers[:,1],marker='^',s=500,c=colourindexes,cmap=plt.cm.plasma)
+    ax.set_title('Iteration'+str(iters))
+    plt.savefig("kmeans"+str(iters)+".png")
+ax.scatter(sess.run(points).transpose()[0],sess.run(points).transpose()[1],marker='o',s=200,c=assignments,cmap=plt.cm.coolwarm)
+plt.show()
+end=time.time()
+print("Found in %.2f seconds" %(end-start)),iters,"iterations"
+print("Centroids:")
+print(centers)
+print("Cluster assignments:",assignments)
